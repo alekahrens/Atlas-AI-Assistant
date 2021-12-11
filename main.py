13249da85194d2f27ec3488174b0bacca057700f
alekahrens <scaaja@gmail.com>
2021-12-11 16:46:46 -0500

    Update from 12/9/21
    Added in google calendar and a few other changes

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import datetime
+from __future__ import print_function
 import webbrowser
+from re import search
+
 import speech_recognition
 import pyttsx3
 import wikipedia
 import time
-
+import datetime
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from tkinter import *
+from  tkinter import ttk
 
 engine = pyttsx3.init()
 webbrowser.register('chrome', None)
-first = True
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def speak(audio):
     engine.say(audio)
@@ -19,6 +28,36 @@ def computerTime():
     Time = datetime.datetime.now().strftime("%H:%M:%S")
     speak(Time)
 
+def events():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    print('Events for today')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                          maxResults=1, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        speak('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+        speak(start)
+        speak(event['summary'])
 
 def date():
     year = int(datetime.datetime.now().year)
@@ -92,13 +131,6 @@ def wakeCommand():
 
 def actions():
     statement = takeCommand().lower()
-    if 'wikipedia' in statement:
-        speak('Searching Wikipedia...')
-        statement = statement.replace("wikipedia", "")
-        results = wikipedia.summary(statement, sentences=3)
-        speak("According to Wikipedia")
-        print(results)
-        speak(results)
     if 'open youtube' in statement:
         webbrowser.open_new_tab("https://www.youtube.com")
         speak("youtube is open now")
@@ -115,11 +147,14 @@ def actions():
         strTime = datetime.datetime.now().strftime("%H:%M:%S")
         speak(f"the time is {strTime}")
     if 'news' in statement:
-        webbrowser.open_new_tab("https://www.cnn.com")
-        speak('Here are some headlines from cnn,Happy reading')
+        webbrowser.open_new_tab("https://www.bbc.com")
+        speak('Here are some headlines from BBC')
         time.sleep(6)
     if 'time' in statement:
         computerTime()
+    if 'calendar' in statement:
+        events()
+
 
 if __name__=="__main__":
     while True:
